d26e38a957c655e06f55552b140ff7196fd6aab0
Chris-xsg <2682135231@qq.com>
2020-07-13 18:36:10 +0800

    数据库+mkdir

---
+++ OJ-day37/OJ-day37/test.cpp
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+using namespace std;
+
+//数据库连接池
+void Cls_ConnectCount(int n)
+{
+	int count = 0, max_count = 0;
+	while (n--)
+	{
+		string id = "", op = "";
+		cin >> id >> op;
+		if (op == "connect")
+		{
+			count++;
+		}
+		if (op == "disconnect")
+			count--;
+		if (count > max_count)
+			max_count = count;
+	}
+	cout << max_count << endl;
+
+}
+
+//mkdir
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		vector<string> list(n);
+		vector<bool> flag(n, true);
+		for (int i = 0; i < n; ++i) cin >> list[i];
+		sort(list.begin(), list.end());
+
+		for (int i = 0; i < list.size() - 1; ++i)
+		{
+			// 1、两串相同
+			// 2、前串是后串的子串，而且后串后一位是 '/'
+			if (list[i] == list[i + 1]) flag[i] = false;
+			else if (list[i].size() < list[i + 1].size() && 
+				list[i] == list[i + 1].substr(0, list[i].size()) && list[i + 1][list[i].size()] == '/')
+				flag[i] = false;
+		}
+
+		for (int i = 0; i < list.size(); ++i)
+		if (flag[i]) cout << "mkdir -p " << list[i] << endl;
+		cout << endl;
+	}
+	return 0;
+}
+
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		Cls_ConnectCount(n);
+	}
+
+	return 0;
+}
